142ca9a2641c81b57219a6d24212049c2fac5039
Brandon Blackmon <plusbrandon@gmail.com>
2017-03-26 07:38:57 -0700

    Using models instead.

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ComicBookGallery.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ComicBookGallery.Controllers
@@ -10,18 +11,20 @@ namespace ComicBookGallery.Controllers
     {
         public IActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[] {
+                    new Artist() { Name = "Dan Scott", Role = "Script" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Victor Ramirez", Role = "Inks" },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() { Name = "Chris James", Role = "Letters" }
+                }
             };
-            return View();
+            return View(comicBook);
         }
     }
 }
---
+++ ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+
+        public string SeriesTitle { get; set; }
+
+        public int IssueNumber { get; set; }
+
+        public string DescriptionHtml { get; set; }
+
+        public Artist[] Artists { get; set; }
+
+        public bool Favorite { get; set; }
+
+        public string DisplayText => $"{SeriesTitle} #{IssueNumber}";
+
+        public string CoverImageFileName => $"{SeriesTitle.Replace(' ', '-').ToLower()}-{IssueNumber}.jpg";
+    }
+}
